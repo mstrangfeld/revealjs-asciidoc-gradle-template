d977d0ac963ac8965cc3040b35abc2cd132801f7
marvin.strangfeld <marvin.strangfeld@open-xchange.com>
2019-12-01 13:42:46 +0100

    feat: example presentation with ox-theme

--- README.md
+++
@@ -1,2 +0,0 @@
-# revealjs-asciidoc-gradle-template
-Template to create Reveal.js presentations with Asciidoc and Gradle
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import com.github.salomonbrys.gradle.sass.SassTask
+import org.asciidoctor.gradle.jvm.slides.AsciidoctorJRevealJSTask
+import org.asciidoctor.gradle.jvm.slides.RevealJSOptions
+
+plugins {
+    id("org.asciidoctor.jvm.revealjs") version "3.0.0-alpha.3"
+    id("com.github.salomonbrys.gradle.sass") version "1.2.0"
+}
+
+repositories {
+    jcenter()
+    maven {
+        url = uri("http://rubygems-proxy.torquebox.org/releases")
+    }
+}
+
+val sassTask = tasks.named<SassTask>("sassCompile") {
+    source = fileTree("src/style")
+    outputDir = buildDir.resolve("style")
+}
+
+asciidoctorj {
+    modules.diagram.use()
+}
+
+val asciidocTask = tasks.named<AsciidoctorJRevealJSTask>("asciidoctorRevealJs") {
+    dependsOn(sassTask)
+    setOutputDir(buildDir.resolve("presentation"))
+    revealjsOptions {
+        setCustomThemeLocation(buildDir.resolve("style/ox-theme.css"))
+        setTransitionSpeed(RevealJSOptions.TransitionSpeed.MAX_VALUE)
+        setHighlightJsThemeLocation(file("src/style/dracula.css"))
+        setFragments(true)
+        setPushToHistory(true)
+        setControls(false)
+    }
+    clearSecondarySources()
+    attributes(mutableMapOf(
+            "partials" to file("src/docs/asciidoc/partials/"),
+            "imagesdir" to file("src/docs/asciidoc/images/"),
+            "icons" to "font"
+    ))
+    resources {
+        from(sourceDir) {
+            include("images/**")
+        }
+    }
+}
+
+tasks.build {
+    dependsOn(asciidocTask)
+}
